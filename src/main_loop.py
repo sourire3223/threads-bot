d85c3ff34e864500d44ad891af040470e594300c
sourire3223 <sourire3223@gmail.com>
2025-05-16 21:15:45 +0800

    feat: use timestamp to check same post

---
+++ src/main_loop.py
@@ -0,0 +1,50 @@
+import os
+import time
+from pathlib import Path
+
+import requests
+from dotenv import load_dotenv
+
+from src.screenshot import capture_latest_post_screenshots
+
+load_dotenv()
+WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+THREAD_URLS = [
+    "https://www.threads.com/@paul_pork/",
+    "https://www.threads.com/@paul_pork/replies",
+]
+SCREENSHOT_DIR = Path("screenshots")
+
+INTERVAL_SECONDS = 180  # 每 10 分鐘截圖一次
+
+
+def send_image_to_discord(image_path: Path, webhook_url: str):
+    with image_path.open("rb") as f:
+        files = {"attachment": (image_path, f, "image/jpeg")}
+        response = requests.post(webhook_url, files=files, timeout=10)
+        if response.status_code == 200:
+            print("✅ 圖片已成功發送到 Discord")
+        else:
+            print(f"❌ 發送失敗: {response.status_code}")
+        return response
+
+
+def main_loop():
+    while True:
+        new_post_paths = capture_latest_post_screenshots(
+            SCREENSHOT_DIR, THREAD_URLS[0], n_lookback=5, time_lookback=INTERVAL_SECONDS
+        )
+        new_reply_paths = capture_latest_post_screenshots(
+            SCREENSHOT_DIR, THREAD_URLS[1], n_lookback=5, time_lookback=INTERVAL_SECONDS
+        )
+
+        for post_path in sorted(new_reply_paths + new_post_paths):
+            print("✨ 偵測到新貼文，準備發送")
+            send_image_to_discord(post_path, WEBHOOK_URL)
+
+        print(f"⏳ 等待 {INTERVAL_SECONDS // 60} 分鐘...")
+        time.sleep(INTERVAL_SECONDS)
+
+
+if __name__ == "__main__":
+    main_loop()
---
+++ src/screenshot.py
@@ -0,0 +1,85 @@
+import time
+from datetime import datetime
+from pathlib import Path
+
+from playwright.sync_api import sync_playwright
+
+SELECTOR = "div.x78zum5.xdt5ytf>div.x9f619.x1n2onr6.x1ja2u2z"
+
+
+def post_time_to_int(s: str) -> int:
+    return int(datetime.strptime(s, "%Y-%m-%dT%H:%M:%S.%f%z").timestamp())
+
+
+def capture_latest_post_screenshots(
+    folder: Path, url: str, n_lookback: int = 5, time_lookback: int = 2147483647
+) -> list[Path]:
+    """擷取最新的 n 張，且時間在 current_time - time_lookback 之後的截圖"""
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) "
+            "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1",
+            viewport={"width": 390, "height": 1688},  # height for long post
+            device_scale_factor=3,
+            is_mobile=True,
+            has_touch=True,
+        )
+        page = context.new_page()
+        page.goto(url)
+        page.wait_for_timeout(3000)
+
+        try:
+            page.wait_for_selector(SELECTOR, timeout=3000)
+            # remove dialog
+            page.evaluate("""
+                document.querySelectorAll('div[role="dialog"]').forEach(el => {
+                    el.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.remove();
+                });
+            """)
+
+            screenshot_paths = []
+            current_time = int(time.time())
+
+            for i in range(n_lookback):
+                post = page.locator(SELECTOR).nth(i)
+                # remove "Threads terms of use" alert
+                page.evaluate("""
+                    document.querySelectorAll('div[role="alert"]').forEach(el => {
+                        el.parentElement?.parentElement?.parentElement?.parentElement?.remove();
+                    });
+                """)
+                datetimes = post.evaluate("""
+                    (post) => Array.from(post.querySelectorAll('time'))
+                        .map(el => el.getAttribute('datetime'))
+                """)
+
+                time_s = post_time_to_int(max(datetimes))
+                print(time_s, current_time - time_lookback)
+                if time_s < current_time - time_lookback:
+                    print(f"❌ 時間不符合: {time_s} > {current_time - time_lookback}")
+                    continue
+
+                path = folder / f"{time_s}.png"
+                if path.exists():
+                    print(f"❌ 檔案已存在: {path}")
+                    continue
+
+                post.screenshot(path=path)
+                screenshot_paths.append(path)
+                print(f"✅ 擷取成功: {path}")
+        except Exception as e:
+            print("❌ 擷取失敗:", e)
+        finally:
+            browser.close()
+
+        return screenshot_paths
+
+
+if __name__ == "__main__":
+    # 確保目錄存在
+    screenshot_dir = Path("screenshots")
+    screenshot_dir.mkdir(parents=True, exist_ok=True)
+
+    # 擷取最新的 n 張截圖
+    capture_latest_post_screenshots(screenshot_dir, "https://www.threads.com/@paul_pork/replies", n_lookback=7)
